30e4d4c0c1321c3e260371611e76965051337d22
Brian Swank <brianswank@gmail.com>
2020-05-09 15:32:46 -0400

    use design system

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
 import DefaultLayout from '~/layouts/Default.vue'
-import '~/assets/scss/global.scss'
+import '@swankdev/design'
 import '~/assets/scss/syntax.scss'
 
 export default function(Vue) {
