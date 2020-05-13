f33d33463722089fc81becd5338c95c7bd8410cc
Brian Swank <brianswank@gmail.com>
2020-05-13 10:58:21 -0400

    add twitter card meta tags

--- src/main.js
+++ src/main.js
@@ -3,8 +3,20 @@ import '@bswank/design'
 import '~/assets/scss/global.scss'
 import '~/assets/scss/syntax.scss'
 
-export default function(Vue) {
+export default function(Vue, { head }) {
+  head.meta.push(
+    {
+      name: 'twitter:card',
+      content: 'summary'
+    },
+    {
+      name: 'twitter:creator',
+      content: '@briansw'
+    }
+  )
+
   Vue.component('Layout', DefaultLayout)
+
   Vue.mixin({
     data() {
       return {
