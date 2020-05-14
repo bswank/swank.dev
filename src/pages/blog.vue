57cd0eb18355618046cae88367f91a202c299a82
Brian Swank <brianswank@gmail.com>
2020-05-14 14:44:19 -0400

    Revert "remove og meta tags"
    This reverts commit 236e2bd10f60b79eec8f35ed77e80712cd3bc5c6.

--- src/pages/blog.vue
+++ src/pages/blog.vue
@@ -18,7 +18,17 @@ export default {
   },
   metaInfo: {
     title: 'Weblog',
-    description: `Remember when blogs were weblogs and blogspot was the newest, coolest thing? Crazy.`
+    description: `Remember when blogs were weblogs and blogspot was the newest, coolest thing? Crazy.`,
+    meta: [
+      {
+        property: 'og:title',
+        content: 'Weblog - Swank.dev'
+      },
+      {
+        property: 'og:description',
+        content: `Remember when blogs were weblogs and blogspot was the newest, coolest thing? Crazy.`
+      }
+    ]
   },
   data() {
     return {
