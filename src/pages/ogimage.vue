bd18c0e0f50b5bfc7860953244849677e472fbb0
Brian Swank <brianswank@gmail.com>
2020-05-15 23:23:42 -0400

    give it a try: ogimage gen function

---
+++ src/pages/ogimage.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="layout">
+    <h2>
+      <span>{{ $route.query.category }}</span>
+    </h2>
+    <h1>
+      <span>{{ $route.query.title }}</span>
+    </h1>
+    <h2 style="flex: 1;">
+      <span v-for="tag in $route.query.tags.split(',')" :key="tag">
+        #{{ tag }}&nbsp;
+      </span>
+    </h2>
+    <Logo class="fixed-logo" />
+  </div>
+</template>
+
+<script>
+import Logo from '@/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.layout {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  background-color: var(--alt);
+  width: 1200px;
+  height: 630px;
+  padding: 75px;
+
+  h1 {
+    line-height: 1.4;
+
+    span {
+      font-size: 2em;
+    }
+  }
+
+  h2 {
+    margin: 0;
+
+    span {
+      font-size: 1.3em;
+      background: none;
+      color: var(--primary);
+    }
+  }
+
+  .fixed-logo {
+    line-height: 1;
+    position: absolute;
+    bottom: 50px;
+    right: 50px;
+  }
+}
+</style>
