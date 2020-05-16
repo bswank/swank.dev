c245b97bf53ee13e6d12f6151f602ba52f2a36c5
Brian Swank <brianswank@gmail.com>
2020-05-16 15:47:08 -0400

    try moving edit link to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,28 @@
 <template>
   <footer>
-    <span>
-      &copy; 2020 Brian Swank -
-      <a target="_blank" href="https://github.com/bswank/swank.dev">
-        Source
-      </a>
+    <span class="footer-items">
+      &copy; 2020 Brian Swank /
+      <a target="_blank" href="https://github.com/bswank/swank.dev"> Source</a>
+      <span v-if="$route.path.includes('/blog/') && $route.path.length > 6">
+        /
+        <a target="_blank" :href="editURL"> Edit</a>
+      </span>
     </span>
   </footer>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      editURL: ''
+    }
+  },
+  created() {
+    const mdFile = this.$route.params.slug
+    this.editURL = `https://github.com/bswank/swank.dev/edit/master/content/posts/${mdFile}.md`
+  }
+}
 </script>
 
 <style lang="scss">
@@ -18,7 +30,7 @@ footer {
   margin: 0 var(--big-space) var(--big-space) var(--big-space);
   text-align: center;
 
-  span {
+  span.footer-items {
     font-size: 0.75em;
     font-weight: 300;
   }
