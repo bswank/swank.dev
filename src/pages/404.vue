7e9a5b8f55a95bf5c856b212fcfd850917a596de
Brian Swank <brianswank@gmail.com>
2020-05-15 23:22:58 -0400

    add a pithy 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,12 @@
+<template>
+  <Layout>
+    <h1><span>404</span></h1>
+    <h2>
+      <span>The thing you were looking for is not where you looked.</span>
+    </h2>
+    <p>
+      I'd tell you this was a technical failure, but the truth is, I change my
+      mind a lot and I probably moved whatever you're looking for. Sorry.
+    </p>
+  </Layout>
+</template>
